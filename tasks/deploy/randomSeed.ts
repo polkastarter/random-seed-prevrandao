22df1aada049ee0e020260b9673a15c33016c27e
Sven Meyer <git_dev@svenmeyer.com>
2022-05-03 01:07:55 +1000

    fix: deploy task , get chainId

--- tasks/deploy/randomSeed.ts
+++ tasks/deploy/randomSeed.ts
@@ -1,14 +1,18 @@
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { task } from "hardhat/config";
-import { TaskArguments } from "hardhat/types";
+import type { TaskArguments } from "hardhat/types";
 
-import { RandomSeed } from "../../src/types/RandomSeed";
-import { RandomSeed__factory } from "../../src/types/factories/RandomSeed__factory";
+import type { RandomSeed } from "../../src/types/RandomSeed";
+import type { RandomSeed__factory } from "../../src/types/factories/RandomSeed__factory";
 
 task("deploy:RandomSeed")
   // .addParam("parameterName", "value")
   .setAction(async function (taskArguments: TaskArguments, { ethers }) {
+    const signers: SignerWithAddress[] = await ethers.getSigners();
+    const chainId = await signers[0].getChainId();
+    console.log("deploying to network with chainId :", chainId);
     const randomseedFactory: RandomSeed__factory = <RandomSeed__factory>await ethers.getContractFactory("RandomSeed");
-    const randomseed: RandomSeed = <RandomSeed>await randomseedFactory.deploy(); // (taskArguments.parameterName);
+    const randomseed: RandomSeed = <RandomSeed>await randomseedFactory.connect(signers[0]).deploy(); // (taskArguments.parameterName);
     await randomseed.deployed();
     console.log("RandomSeed deployed to: ", randomseed.address);
   });
